30a96c5795a6ed840185b47e072a3c9b229054db
장준석 <evereke@gmail.com>
2021-03-27 15:22:06 +0900

    테스트버튼 2 추가

--- app/src/main/java/com/nepplus/kotlinbasic_20210327/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20210327/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        testBtn2.setOnClickListener {
+            Log.e("메인화면로그","테스트2 눌림")
+        }
+
         testBtn1.setOnClickListener {
 
 //            Ctrl + / = 코드에 메모 (주석) 남기기
